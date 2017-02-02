9af8833fbdeb9ada8e68ed3e1fe93067e8ab0d8b
Holpa <ahmadholpa@cmail.carleton.ca>
2017-02-02 14:06:57 -0500

    lab 4 Done no controller

---
+++ src/main/java/hello/BuddyInfo.java
@@ -0,0 +1,47 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+/**
+ * Created by ahmadholpa on 1/12/2017.
+ */
+
+@Entity
+public class BuddyInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String phoneNumber;
+
+    protected BuddyInfo() {}
+    public BuddyInfo(String name, String phoneNumber) {
+            this.name= name;
+            this.phoneNumber = phoneNumber;
+    }
+    public String getName()
+        {
+            return this.name;
+        }
+    public String getPhoneNumber()
+        {
+            return this.phoneNumber;
+        }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        return String.format(
+                "buddy[id=%d, firstName='%s', phoneNumber='%s']",
+                id, name, phoneNumber);
+    }
+}
+
