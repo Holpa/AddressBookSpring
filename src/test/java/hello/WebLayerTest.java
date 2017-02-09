003a47329608657f5baf3360f028f6d9bc01390d
Holpa <ahmadholpa@cmail.carleton.ca>
2017-02-09 11:09:49 -0500

    About to add travis and dynamic integration

--- src/test/java/hello/WebLayerTest.java
+++ src/test/java/hello/WebLayerTest.java
@@ -1,6 +1,7 @@
 package hello;
 
 /*
+
 import static org.junit.matchers.JUnitMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
