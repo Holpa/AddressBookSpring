43b1139adb7588ae03cd4986a8430d4114793095
Holpa <ahmadholpa@cmail.carleton.ca>
2017-02-09 12:47:28 -0500

    About to add travis and dynamic integration

--- src/main/java/hello/Addressbook.java
+++ src/main/java/hello/Addressbook.java
@@ -1,7 +1,27 @@
 package hello;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ahmadholpa on 2/9/2017.
  */
+@Entity
 public class Addressbook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+
+    public Addressbook()
+    {
+
+    }
+
+
 }
