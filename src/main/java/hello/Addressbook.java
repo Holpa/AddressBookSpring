878110d370aa74ef97503544face7b076348f73b
Holpa <ahmadholpa@cmail.carleton.ca>
2017-02-09 13:03:59 -0500

    About to add travis and dynamic integration

--- src/main/java/hello/Addressbook.java
+++ src/main/java/hello/Addressbook.java
@@ -1,27 +1,37 @@
 package hello;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by ahmadholpa on 2/9/2017.
+ * it does nothing so far
  */
+
 @Entity
 public class Addressbook {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
+    @OneToMany
+    private List<BuddyInfo> biL;
 
 
     public Addressbook()
     {
-
+        biL= new ArrayList<>();
     }
 
+    public void addBuddy(BuddyInfo bi)
+    {
+        biL.add(bi);
+    }
+    public List<BuddyInfo> getBuddyInfoList()
+    {
+        return this.biL;
+    }
 
 }
